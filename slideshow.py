b49c0eb2f6a6a81b371012b88c1359fd98f442b1
SmashedFrenzy16 <smashedfrenzy16@gmail.com>
2024-08-28 19:43:04 +0100

    Update slideshow.py with main code

--- slideshow.py
+++ slideshow.py
@@ -6,4 +6,26 @@ root = Tk()
 
 root.title("Slideshow Widget")
 
+image1 = ImageTk.PhotoImage(Image.open("image1.jpg"))
+image2 = ImageTk.PhotoImage(Image.open("image2.jpeg"))
+image3 = ImageTk.PhotoImage(Image.open("image3.jpg"))
+
+image_arr = [image1, image2, image3]
+
+
+pic_label = Label(root)
+
+pic_label.pack()
+
+
+while True:
+
+    for i in range(len(image_arr)):
+
+        pic_label.config(image=image_arr[i])
+
+        root.update()
+
+        time.sleep(2)
+
 root.mainloop()
